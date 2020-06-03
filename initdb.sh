eaac8f85dba5fa0d43e24f06af591df948c6e720
Sergii Shapoval <sergii@firebolt.io>
2020-06-03 12:43:56 +0300

    add installation of psql driver

--- initdb.sh
+++ initdb.sh
@@ -21,5 +21,8 @@ psql boilbench -f schema.sql --quiet
 # Install SQLBoiler
 go get github.com/volatiletech/sqlboiler
 
+# Install SQLBoiler PSQL driver
+go get -u -t github.com/volatiletech/sqlboiler/drivers/sqlboiler-psql
+
 # Generate models
 sqlboiler --wipe --no-context --output ./models psql -t "db"
